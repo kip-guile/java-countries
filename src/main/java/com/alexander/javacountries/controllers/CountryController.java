d16026982a88db3e57e09a455fe6c06fa9392db7
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-09-03 15:13:43 +0100

    second endpoint

--- src/main/java/com/alexander/javacountries/controllers/CountryController.java
+++ src/main/java/com/alexander/javacountries/controllers/CountryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,12 +17,31 @@ public class CountryController {
     @Autowired
     CountryRepository comrepos;
 
+    private List<Country> findCountries(List<Country> myList, CheckCountry tester) {
+        List<Country> tempList = new ArrayList<>();
+        for (Country c : myList) {
+            if (tester.test(c)) {
+                tempList.add(c);
+            }
+        }
+        return tempList;
+    }
+
     //http://localhost:2019/names/all
     @GetMapping(value="names/all", produces = {"application/json"})
     public ResponseEntity<?> ListAllCountries() {
         List<Country> myList = new ArrayList<>();
         comrepos.findAll().iterator().forEachRemaining(myList::add);
-        myList.sort((e1, e2) -> e1.getName().compareToIgnoreCase(e2.getName()));
+        myList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
         return new ResponseEntity<>(myList, HttpStatus.OK);
+    }
+
+    //http://localhost:2019/names/start/u
+    @GetMapping(value = "names/start/{letter}", produces = {"application/json"})
+    public ResponseEntity<?> ListAllByFirstName(@PathVariable char letter) {
+        List<Country> myList = new ArrayList<>();
+        comrepos.findAll().iterator().forEachRemaining(myList::add);
+        List<Country> resList = findCountries(myList, c -> c.getName().charAt(0) == letter);
+        return new ResponseEntity<>(resList, HttpStatus.OK);
     }
 }
